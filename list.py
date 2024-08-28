be075e71fb860da708ccf22cd4e86bd7be5a53ec
Arpit Kumar <154455719+arpitvirus@users.noreply.github.com>
2024-08-28 11:23:10 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+# Write a Python program to find the largest and 
+# smallest elements in a list.
+
+list = [9,5,33,5,5,6,7]
+list.sort()
+print("The lowest value of list is:",list[0])
+b = len(list)
+print("The biggest Number is:",list[b-1])
